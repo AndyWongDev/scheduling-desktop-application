ef5799c232bbf0c8f94dc6769280a164a9702864
Andy Wong <andywong.ny@gmail.com>
2020-12-07 22:10:47 -0500

    MainMenuController views

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -2,17 +2,24 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.ToggleGroup;
+import javafx.stage.Stage;
 import model.Appointment;
 
+import java.io.IOException;
 import java.net.URL;
-import java.time.*;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
@@ -20,6 +27,9 @@ public class MainMenuController implements Initializable {
     ZonedDateTime zonedDateTime = ZonedDateTime.now();
     LocalDateTime localDateTime = zonedDateTime.now().toLocalDateTime();
     LocalDateTime utcDateTime = zonedDateTime.now(ZoneId.of("UTC")).toLocalDateTime();
+
+    Stage stage;
+    Parent scene;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -72,6 +82,9 @@ public class MainMenuController implements Initializable {
     private TableColumn<Appointment, String> locationCol;
 
     @FXML
+    private TableColumn<Appointment, String> contactCol;
+
+    @FXML
     private TableColumn<Appointment, String> typeCol;
 
     @FXML
@@ -81,23 +94,38 @@ public class MainMenuController implements Initializable {
     private TableColumn<Appointment, LocalDateTime> endCol;
 
     @FXML
-    void onActionAddAppointmentButton(ActionEvent event) {
+    private TableColumn<Appointment, Integer> customerIdCol;
 
+    @FXML
+    void onActionAddAppointmentButton(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/AddAppointment.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void onActionAddCustomerButton(ActionEvent event) {
-
+    void onActionAddCustomerButton(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/AddCustomer.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void onActionModifyAppointmentButton(ActionEvent event) {
-
+    void onActionModifyAppointmentButton(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/ModifyAppointment.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
-    void onActionModifyCustomerButton(ActionEvent event) {
-
+    void onActionModifyCustomerButton(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/ModifyCustomer.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
