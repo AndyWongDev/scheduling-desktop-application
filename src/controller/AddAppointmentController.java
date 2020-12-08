bc6ff2aee72d156913dae76cbec351a7898d873f
Andy Wong <andywong.ny@gmail.com>
2020-12-07 22:14:16 -0500

    included onCancel actions

--- src/controller/AddAppointmentController.java
+++ src/controller/AddAppointmentController.java
@@ -2,12 +2,21 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AddAppointmentController {
+
+    Stage stage;
+    Parent scene;
 
     @FXML
     private TextField idText;
@@ -49,8 +58,11 @@ public class AddAppointmentController {
     private Button saveButton;
 
     @FXML
-    void onActionCancelButton(ActionEvent event) {
-
+    void onActionCancelButton(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
--- src/controller/AddCustomerController.java
+++ src/controller/AddCustomerController.java
@@ -2,11 +2,20 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class AddCustomerController {
+
+    Stage stage;
+    Parent scene;
 
     @FXML
     private TextField nameText;
@@ -33,8 +42,11 @@ public class AddCustomerController {
     private Button saveButton;
 
     @FXML
-    void onActionCancelButton(ActionEvent event) {
-
+    void onActionCancelButton(ActionEvent event) throws IOException {
+        stage = (Stage) ((Button) event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
