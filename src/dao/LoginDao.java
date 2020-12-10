1ca87073f1e83d6b3c4309c16e3221b1c7acfc41
Andy Wong <andywong.ny@gmail.com>
2020-12-09 22:40:20 -0500

    added login history tracker

--- src/dao/LoginDao.java
+++ src/dao/LoginDao.java
@@ -3,11 +3,16 @@ package dao;
 import model.User;
 import utils.DBConnection;
 import utils.DBQuery;
+import utils.TimezoneUtil;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.sql.*;
+import java.util.Arrays;
+import java.util.List;
 
 public class LoginDao {
     private static Connection connection = DBConnection.getConnection();
@@ -40,12 +45,37 @@ public class LoginDao {
                 int id = resultSet.getInt("User_ID");
                 currentUser = new User(id, username, password);
                 System.out.println("Username and password found: " + currentUser.getName());
+                loginAttemptTracker(true, currentUser.getName());
                 return true;
             }
         } catch (SQLException e) {
             e.printStackTrace();
         }
+        loginAttemptTracker(false, username);
         System.out.println("No username and password found.");
         return false;
+    }
+
+    /**
+     * C. Write code that provides the ability to track user activity by recording all user log-in attempts, dates,
+     * and time stamps and whether each attempt was successful in a file named login_activity.txt.
+     * Append each new record to the existing file, and save to the root folder of the application.
+     *
+     * @param status
+     * @param username
+     */
+    private static void loginAttemptTracker(Boolean status, String username) {
+        String result = status ? "SUCCESS" : "FAILED";
+        Timestamp timestamp = TimezoneUtil.getUTCTime();
+        List<String> output = Arrays.asList(result + ": " + username + ": " + timestamp + " UTC");
+
+        try {
+            Files.write(Paths.get("login_activity.txt"), output,
+                    StandardCharsets.UTF_8,
+                    StandardOpenOption.CREATE,
+                    StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
